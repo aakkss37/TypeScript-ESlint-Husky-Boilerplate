831c71a6b4dedc212919cb7e74b0488fa34e2195
amar kumar <amarkumar.sharma.124@gmail.com>
2023-06-09 13:51:03 +0530

    ESlint setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Example from "./example";
+function App() {
+	return (
+		<div>
+			<h1>hello rosterly</h1>
+			<Example />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/example.tsx
@@ -0,0 +1,5 @@
+const Example: React.FC = () => {
+	return <div>example</div>;
+};
+
+export default Example;
